ff77e2ed651b62ba6758964426dd918a34ef44b8
Cheikh-a44 <cheikhahmedoumedlemine@gmail.com>
2026-05-17 08:29:42 +0100

    vue project

---
+++ vue-project/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h1>Vue Components</h1>
+
+    <MyButton />
+  </div>
+</template>
+
+<script setup>
+import MyButton from './components/MyButton.vue'
+</script>
+
+<style>
+.container {
+  text-align: center;
+  margin-top: 100px;
+}
+</style>
---
+++ vue-project/src/components/MyButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button @click="increase">
+    Clicked {{ count }} times
+  </button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increase() {
+  count.value++
+}
+</script>
+
+<style scoped>
+button {
+  padding: 12px 20px;
+  font-size: 18px;
+  background: #42b883;
+  color: white;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+}
+</style>
